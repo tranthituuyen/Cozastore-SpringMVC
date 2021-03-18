4d7eb2470d9f470c8f19b5291e7c65fac4065eb6
tranthituuyen <73946720+tranthituuyen@users.noreply.github.com>
2021-03-18 19:45:08 +0700

    reverse commit

--- Cozastore/src/main/java/vn/edu/nlu/constant/SystemConstant.java
+++ Cozastore/src/main/java/vn/edu/nlu/constant/SystemConstant.java
@@ -5,7 +5,4 @@ public class SystemConstant {
     public static final boolean INACTIVE_STATUS = false;
 
     public static final String MODEL = "model";
-
-    // packages
-    public static final String REPOSITORY_PACKAGE = "vn.edu.nlu.repository";
 }
--- Cozastore/src/main/java/vn/edu/nlu/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package vn.edu.nlu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vn.edu.nlu.entity.ProductEntity;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-
-}
